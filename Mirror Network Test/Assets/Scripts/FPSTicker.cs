3f4dce5f9b491f35be3c192292eb3adb0114a9a8
Денис Бондаренко <denys.bondarenko@nure.ua>
2021-03-12 11:33:22 +0200

    Mirror Network Test project

---
+++ Mirror Network Test/Assets/Scripts/FPSTicker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSTicker : MonoBehaviour
+{
+    [SerializeField] private Text text;
+    [SerializeField] private float time;
+
+    private void Update()
+    {
+        time += (Time.deltaTime - time) * 0.1f;
+        float fps = 1.0f / time;
+        text.text = "FPS: " + Mathf.Ceil(fps);
+    }
+}
---
+++ Mirror Network Test/Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+    [SerializeField, SyncVar(hook = nameof(OnChangeColor))] private Color myColor;
+    [SerializeField] private float moveSpeed = 10f;
+
+    private MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        myColor = meshRenderer.material.color;
+    }
+
+    private void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(horizontal, 0f, vertical) * moveSpeed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdChangeColor();
+        }
+    }
+
+    [Command]
+    private void CmdChangeColor()
+    {
+        myColor = UnityEngine.Random.ColorHSV();
+    }
+
+
+    private void OnChangeColor(Color oldColor, Color newColor)
+    {
+        meshRenderer.material.color = newColor;
+    }
+}
