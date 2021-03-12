bde450d99d89641eb8fceb9508a84ef19791a1c3
Денис Бондаренко <denys.bondarenko@nure.ua>
2021-03-12 18:04:29 +0200

    Main, Lobby scenes added

---
+++ Mirror Network Test/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.UI;
+
+public class GameManager : NetworkBehaviour
+{
+    [SerializeField] private Text playersTxt;
+
+    [SyncVar(hook = nameof(OnUpdateTextConnected))] public int playersConnected;
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        playersConnected += 1;
+        
+    }
+
+    private void OnUpdateTextConnected(int oldValue, int newValue)
+    {
+        playersTxt.text = "Players connected: " + newValue;
+    }
+}
