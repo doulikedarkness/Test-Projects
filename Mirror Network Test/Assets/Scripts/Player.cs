73781cffb5c170fd32061ee46c1bca967e324838
Денис Бондаренко <denys.bondarenko@nure.ua>
2021-03-12 15:29:16 +0200

    [Mirror Test] Добавлен скилл увеличения, исправлена ошибка с канвасом

--- Mirror Network Test/Assets/Scripts/Player.cs
+++ Mirror Network Test/Assets/Scripts/Player.cs
@@ -2,13 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using UnityEngine.UI;
 
 public class Player : NetworkBehaviour
 {
     [SerializeField, SyncVar(hook = nameof(OnChangeColor))] private Color myColor;
     [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private int attackCooldown = 8;
+    [SerializeField] private Text cdText;
+    [SerializeField] private Button buttonSkill;
+    [SerializeField] private Canvas canvas; 
+
+    private float cd = 1;
 
     private MeshRenderer meshRenderer;
+
+    public override void OnStartLocalPlayer()
+    {
+        canvas.enabled = true;
+    }
 
     private void Awake()
     {
@@ -21,15 +33,9 @@ public class Player : NetworkBehaviour
         if (!isLocalPlayer)
             return;
 
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        transform.position += new Vector3(horizontal, 0f, vertical) * moveSpeed * Time.deltaTime;
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            CmdChangeColor();
-        }
+        CooldownTimer();
+        Movements();
+        ActionInputs();
     }
 
     [Command]
@@ -42,5 +48,50 @@ public class Player : NetworkBehaviour
     private void OnChangeColor(Color oldColor, Color newColor)
     {
         meshRenderer.material.color = newColor;
+    }
+
+    private void Movements()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(horizontal, 0f, vertical) * moveSpeed * Time.deltaTime;
+    }
+
+    private void ActionInputs()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            CmdChangeColor();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnAttack();
+        }
+    }
+
+    private void OnAttack()
+    {
+        if (cd <= 1)
+        {
+            transform.localScale += new Vector3(0.5f, 0.5f, 0.5f);
+            cd = attackCooldown;
+            buttonSkill.interactable = false;
+        }
+    }
+
+    private void CooldownTimer()
+    {
+        if (cd <= attackCooldown && cd > 1)
+        {
+            cd -= Time.deltaTime;
+            cdText.text = ((int)cd).ToString();
+        }
+        else if (cd <= 1)
+        {
+            cdText.text = string.Empty;
+            buttonSkill.interactable = true;
+        }
     }
 }
